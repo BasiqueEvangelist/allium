83364b207009c1fca94f2e0172151ae1c2e80bac
Basique <basiqueevangelist@yandex.ru>
2024-03-03 00:44:12 +0300

    add a bit more docs (definitely not to test that fields work)

--- src/main/java/dev/hugeblank/allium/lua/api/recipe/RecipeLib.java
+++ src/main/java/dev/hugeblank/allium/lua/api/recipe/RecipeLib.java
@@ -12,19 +12,36 @@ import org.squiddev.cobalt.LuaError;
 
 import java.util.Map;
 
+/**
+ * Library for modifying, adding and removing recipes with Lua.
+ */
 @LuaWrapped(name = "recipe")
 public class RecipeLib implements WrappedLuaLibrary {
     private static boolean IN_MODIFY_PHASE = false;
 
+    /**
+     * Event for adding new recipes.
+     */
     @LuaWrapped
     public static final SimpleEventType<AddRecipesContext.Handler> ADD = new SimpleEventType<>(new Identifier("allium:recipe/adding_recipes"));
 
+    /**
+     * Event for modifying and replacing recipes.
+     *
+     * <p>Setting fields of recipes is only allowed in this phase.
+     */
     @LuaWrapped
     public static final SimpleEventType<ModifyRecipesContext.Handler> MODIFY = new SimpleEventType<>(new Identifier("allium:recipe/modifying_recipes"));
 
+    /**
+     * Event for removing recipes.
+     */
     @LuaWrapped
     public static final SimpleEventType<RemoveRecipesContext.Handler> REMOVE = new SimpleEventType<>(new Identifier("allium:recipe/remove_recipes"));
 
+    /**
+     * All registered recipe types
+     */
     @LuaWrapped(name = "types")
     public static final @CoerceToBound RecipeTypeLib TYPES = new RecipeTypeLib();
 
